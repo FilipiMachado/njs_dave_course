8ca6a4d92e3685114cb57256f3f59e9e0d35e2be
filipi <filipisee@hotmail.com>
2023-09-27 23:00:35 -0300

    added fetch data on lib && added data types

--- next02/app/users/page.tsx
+++ next02/app/users/page.tsx
@@ -1,11 +1,34 @@
 import type { Metadata } from "next";
+import getAllUsers from "@/lib/getAllUsers";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Users",
 };
 
-export default function UsersPage() {
-  return (
-    <div>Users Page</div>
-  )
+export default async function UsersPage() {
+  const usersData: Promise<User[]> = getAllUsers();
+
+  const users = await usersData;
+
+  const content = (
+    <section>
+      <h2>
+        <Link href="/">Back to Home Page</Link>
+      </h2>
+      <br />
+      {users.map((user) => {
+        return (
+          <>
+            <p key={user.id}>
+              <Link href={`/users/${user.id}`}>{user.name}</Link>
+            </p>
+            <br />
+          </>
+        );
+      })}
+    </section>
+  );
+
+  return content;
 }
