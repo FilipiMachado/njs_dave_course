d6101b65921b616fe591a5064047d8c15d3b71cb
filipi <filipisee@hotmail.com>
2023-09-28 11:57:12 -0300

    testing shadcn components

--- nextjs_tests/app/page.tsx
+++ nextjs_tests/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <>
       <h1>Main Page</h1>
-      <Button>Button</Button>
+      <div className="bg-slate-50 p-10">
+      <Button variant={"secondary"}>Button</Button>
+      </div>
+      <Input type="text" placeholder="Email"/>
     </>
   )
 }
