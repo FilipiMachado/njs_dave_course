962378e0ac68f21030a4fdd0f9190e8bb404a6c5
filipi <filipisee@hotmail.com>
2023-09-27 21:07:50 -0300

    testing next routing

--- next01/app/layout.tsx
+++ next01/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>My Navbar</h1>
+        <h1>Navbar should stay here</h1>
         {children}
       </body>
     </html>
