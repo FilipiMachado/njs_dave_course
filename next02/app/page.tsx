a4457d633eef59209112abbfb55ba9152d3eaa75
filipi <filipisee@hotmail.com>
2023-09-27 22:31:15 -0300

    structuring fetch data

--- next02/app/page.tsx
+++ next02/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      Home Page
+      <h1>Home Page</h1>
+      <p>
+        <Link href="/users">Users</Link>
+      </p>
     </main>
   );
 }
